44ea085d30d9dd5e4b074d7876dd2b98fbead7ba
iBenzo <secretauditore@gmail.com>
2022-09-27 20:48:16 -0400

    Added the round function to work and keep track of the winner and add it to the score count

--- style.js
+++ style.js
@@ -12,23 +12,6 @@ function getComputerChoice() {
     return randomComputerSelection
 }
 
-function getPlayerChoice() {
-    let playerChoice = prompt('Pick either "rock", "paper", or "scissors');
-    let updatedPlayerSelection = playerChoice.toLowerCase();
-    if(updatedPlayerSelection == 'rock'){
-        return updatedPlayerSelection;
-    }
-    else if (updatedPlayerSelection == 'paper'){
-        return updatedPlayerSelection
-    }
-    else if (updatedPlayerSelection == 'scissors'){
-        return updatedPlayerSelection
-    }
-    else{
-        getPlayerChoice()
-    }
-}
-
 /* This is the round function to choose a winner and add to the score*/
 
 function playRound(playerSelection, computerSelection) {
@@ -37,29 +20,29 @@ function playRound(playerSelection, computerSelection) {
     }
     else if(playerSelection == 'rock'){
         if(computerSelection == 'paper'){
-            computerScore++
-            return 'Computer Won Round';
+            computerScore++;
+            computerScoreDisplayText.innerText = `Computer score: ${computerScore}`;
         }else{
             playerScore++
-            return 'Player Won Round'
+            playerScoreDisplayText.innerText = `player score: ${playerScore}`; 
         }
     }
     else if(playerSelection == 'paper'){
         if(computerSelection == 'scissors'){
-            computerScore++
-            return 'Computer Won Round';
+            computerScore++;
+            computerScoreDisplayText.innerText = `Computer score: ${computerScore}`;
         }else{
             playerScore++
-            return 'Player Won Round'
+            playerScoreDisplayText.innerText = `player score: ${playerScore}`;
         }
     }
     else if(playerSelection == 'scissors'){
         if(computerSelection == 'rock'){
-            computerScore++
-            return 'Computer Won Round';
+            computerScore++;
+            computerScoreDisplayText.innerText = `Computer score: ${computerScore}`;
         }else{
             playerScore++
-            return 'Player Won Round'
+            playerScoreDisplayText.innerText = `player score: ${playerScore}`;
         }
     }
   }
@@ -78,8 +61,7 @@ function gameWinner(){
 }
 
 /*This just runs the game function*/
-/*game()
-*/
+
 
 /*This creates the buttons action and removes the homepage content*/
 const body = document.body;
@@ -89,6 +71,7 @@ const divThree = document.createElement('div');
 const divFour = document.createElement('div');
 const buttonRock = document.createElement('button');
 
+
 const buttonPaper = document.createElement('button');
 const buttonScissors = document.createElement('button');
 const computerScoreDisplayText = document.createElement('p');
@@ -97,32 +80,61 @@ const head = document.createElement('h1');
 const header = document.querySelector('#title');
 const btn = document.querySelector('#playButton');
 const homePageContainer = document.querySelector('#homePage');
+
 btn.addEventListener('click',() => {
+    //removes first page
     homePageContainer.remove();
     header.remove();
     btn.remove();
-
+    //creates a new page
     body.append(div);
     div.setAttribute('id', 'playPage');
-
+    //sets up the layout for the playing page
     div.append(divTwo);
+    div.append(divFour);
     div.append(divThree);
     divThree.setAttribute('id', 'divThree');
-
     divTwo.append(head);
+    divTwo.setAttribute('id', 'divTwo');
     head.innerText = "Scoreboard";
-    divTwo.append(divFour);
-    
+
     divFour.setAttribute('id', 'divFour');
     divFour.append(computerScoreDisplayText);
-    computerScoreDisplayText.innerText = 'Computer score:';
+    computerScoreDisplayText.innerText = `Computer score: ${computerScore}`;
     divFour.append(playerScoreDisplayText);
-    playerScoreDisplayText.innerText = 'player score:';
+    playerScoreDisplayText.innerText = `player score: ${playerScore}`;
 
     divThree.append(buttonRock);
     buttonRock.setAttribute('id', 'rock');
+    buttonRock.innerText = 'Rock';
     divThree.append(buttonPaper);
     buttonPaper.setAttribute('id', 'paper');
+    buttonPaper.innerText = 'Paper';
     divThree.append(buttonScissors);
     buttonScissors.setAttribute('id', 'scissors');
+    buttonScissors.innerText = 'scissors';    
+
+    //Creates the click action of rock selection
+    const rockBtn = document.querySelector('#rock');
+    rockBtn.addEventListener('click', () =>{
+        let playerSelection = 'rock';
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    })
+
+    //Creates the click action of paper selection
+    const paperBtn = document.querySelector('#paper');
+    paperBtn.addEventListener('click', () =>{
+        let playerSelection = 'paper';
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    })
+
+    //Creates the click action of scissors selection
+    const scissorsBtn = document.querySelector('#scissors');
+    scissorsBtn.addEventListener('click', () =>{
+        let playerSelection = 'scissors';
+        const computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+    })
 })
